b9f2ebb7c8951bef83ffc86370935d9e11d6e83f
jinwooorp <jim8912woo@gmail.com>
2025-04-28 14:57:45 +0900

    2775.cpp

---
+++ 2775/2775.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[15][15];
+
+int main() {
+
+    int t; cin >> t;
+
+    for (int i = 0; i < 15; i++){
+        dp[0][i] = i;
+        dp[i][1] = 1;
+    }
+
+    for (int i = 1; i < 15; i++){
+        for (int j = 2; j < 15; j++){
+            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+    }
+
+    while (t--){
+        int k;
+        int n;
+        cin >> k;
+        cin >> n;
+        cout << dp[k][n] << '\n';
+    }
+
+}
